a6b5c42406f96f796e9e7d0fa9a4011d479d7782
Daisuke Nakahara <daisuken@msn.com>
2021-01-08 08:59:13 -0800

    Remove warnings

--- Deployment/ConfigureTSI.sh
+++ Deployment/ConfigureTSI.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-az extension add --name timeseriesinsights --yes
+az extension add --name timeseriesinsights --yes --only-show-errors
 
 uniqueId=$1
 resGroup=$2
@@ -17,8 +17,11 @@ echo "TSI Env Name  : ${tsiName}"
 subscriptionId=$(az account show --query id -o tsv)
 adAppName='IoTPnPWS-TSI-AD-App'-"$subscriptionId"
 # az login --identity
-servicePrincipalAppId=$(az ad app create --display-name $adAppName --identifier-uris "https://${adAppName}"  --oauth2-allow-implicit-flow true --required-resource-accesses '[{"resourceAppId":"120d688d-1518-4cf7-bd38-182f158850b6","resourceAccess":[{"id":"a3a77dfe-67a4-4373-b02a-dfe8485e2248","type":"Scope"}]}]' --query appId -o tsv)
-#servicePrincipalObjectId=$(az ad sp list --show-mine --query "[?appDisplayName=='$adAppName'].principalObjectId" -o tsv)
+servicePrincipalAppId=$(az ad app list --all --query '[].{AppId:appId}' --display-name $adAppName -o tsv)
+if [ -z $servicePrincipalAppId ]; then
+    servicePrincipalAppId=$(az ad app create --display-name $adAppName --identifier-uris "https://${adAppName}"  --oauth2-allow-implicit-flow true --required-resource-accesses '[{"resourceAppId":"120d688d-1518-4cf7-bd38-182f158850b6","resourceAccess":[{"id":"a3a77dfe-67a4-4373-b02a-dfe8485e2248","type":"Scope"}]}]' --query appId -o tsv)
+fi
+
 servicePrincipalObjectId=$(az ad sp show --id "https://${adAppName}" --query objectId -o tsv)
 if [ -z "$servicePrincipalObjectId" ]; then
     servicePrincipalObjectId=$(az ad sp create --id $servicePrincipalAppId --query objectId -o tsv)
@@ -38,7 +41,7 @@ echo "TSI Client ID : ${temp}"
 temp=$(az webapp config appsettings set --name $webAppName --resource-group $resGroup --settings Azure__TimeSeriesInsights__tsiSecret=$servicePrincipalSecret --query "[?name=='Azure__TimeSeriesInsights__tsiSecret'].[value]" -o tsv)
 echo "TSI Secret    : ${temp}"
 
-temp=$(az timeseriesinsights access-policy create -g $resGroup --environment-name $tsiName -n "TSI-SP" --principal-object-id $servicePrincipalObjectId --roles Reader)
+temp=$(az timeseriesinsights access-policy create -g $resGroup --environment-name $tsiName -n "TSI-SP" --principal-object-id $servicePrincipalObjectId --roles Reader --only-show-errors)
 
 trackerFullUrl="${trackerUrl}?resGroup=${resGroup}&uniqueId=${uniqueId}&progressMarker=4"
 curl -X GET $trackerFullUrl
